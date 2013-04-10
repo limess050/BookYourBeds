09ee2ed640e98c659453c934cb2f7e26594582cb
Phil Stephens <phil@othertribe.com>
2013-04-10 11:26:30 +0100

    added help URIs

--- system/application/config/help_uris.php
+++ system/application/config/help_uris.php
@@ -1,7 +1,39 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['help_uri'] = array(
+							'users/me'				=> 'documentation/edit-your-user-details',
+							'seasons/edit'			=> 'documentation/edit-season',
+							'seasons/create'		=> 'documentation/create-season',
+							'seasons/index'			=> 'documentation/seasons',
+							'settings/bookings'		=> 'documentation/booking-settings',
+							'settings/payments'		=> 'documentation/payment-options',
+							'settings/account'		=> 'documentation/account-settings',
+							'salesdesk/index'		=> 'documentation/sales-desk',
+							'salesdesk/search'		=> 'documentation/sales-desk',
+							'salesdesk/details'		=> 'documentation/sales-desk',
+							'salesdesk/supplements'	=> 'documentation/sales-desk',
+							'salesdesk/confirm'		=> 'documentation/sales-desk',
+							'transfer/start'		=> 'documentation/transfer-booking',
+							'transfer/search'		=> 'documentation/transfer-booking',
+							'transfer/details'		=> 'documentation/transfer-booking',
+							'transfer/supplements'	=> 'documentation/transfer-booking',
+							'transfer/confirm'		=> 'documentation/transfer-booking',
+							'bookings/email'		=> 'documentation/email-booking-details/',
+							'bookings/edit'			=> 'documentation/edit-guest-details',
+							'bookings/show'			=> 'documentation/view-booking',
+							'bookings/search'		=> 'documentation/search-bookings',
+							'supplements/edit'		=> 'documentation/edit-supplement',
+							'supplements/create'	=> 'documentation/create-supplement',
+							'supplements/index'		=> 'documentation/your-supplements',
+							'availability/index'	=> 'documentation/availability',
+							'availability/resource'	=> 'documentation/availability',
+							'resources/supplements'	=> 'documentation/room-supplements',
+							'resources/price'		=> 'documentation/room-pricing',
+							'resources/edit'		=> 'documentation/edit-room',
+							'resources/create'		=> 'documentation/create-room',
+							'resources/index'		=> 'documentation/your-rooms',
+							'bookings/index'		=> 'documentation/diary',
 							'dashboard/index'		=> 'documentation/dashboard',
-							'bookings/index'		=> 'documentation/diary'
-
+							'auth/forgotten_password'	=> 'documentation/forgotten-password',
+							'auth/signin'				=> 'documentation/sign-in'
 							);
