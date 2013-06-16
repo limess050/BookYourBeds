fc3e423d80e59c6951cf3a642c663b048cf3818a
Phil Stephens <phil@othertribe.com>
2013-06-17 10:15:40 +1200

    Added back door domain

--- system/application/controllers/init.php
+++ system/application/controllers/init.php
@@ -12,22 +12,33 @@ class Init extends CI_Controller {
 
 	public function migrate()
 	{
-		$this->load->library('migration');
-
-		if ( ! $this->migration->current())
+		if(in_array($_SERVER['SERVER_NAME'], array('secure.bookyourbeds.dev', 'bybbckdr.bookyourbeds.com')))
 		{
-			show_error($this->migration->error_string());
-		}
+			$this->load->library('migration');
 
-		echo anchor('init/back?v=0', 'Back');
+			if ( ! $this->migration->current())
+			{
+				show_error($this->migration->error_string());
+			}
+
+			echo anchor('init/back?v=0', 'Back');
+		}
 	}
 
 	public function back()
 	{
-		$this->load->library('migration');
+		if(in_array($_SERVER['SERVER_NAME'], array('secure.bookyourbeds.dev', 'bybbckdr.bookyourbeds.com')))
+		{
+			$this->load->library('migration');
 
-		$this->migration->version($this->input->get('v'));
+			$this->migration->version($this->input->get('v'));
 
-		echo anchor('init/migrate', 'Forwards');
+			echo anchor('init/migrate', 'Forwards');
+		}
+	}
+
+	public function host()
+	{
+		echo $_SERVER['HTTP_HOST'];
 	}
 }
