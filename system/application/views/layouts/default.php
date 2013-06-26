9100787cd9f7f082160aa3ff4709a08233ae3f60
Phil Stephens <phil@othertribe.com>
2013-06-27 08:41:19 +1200

    Added GA tracking and newsletter link

--- system/application/views/layouts/default.php
+++ system/application/views/layouts/default.php
@@ -20,7 +20,14 @@ All rights reserved
 	<script type="text/javascript" src="assets/js/bootstrap-transition.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap-modal.js"></script>
 
-	
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-42055908-1', 'bookyourbeds.com');
+	ga('send', 'pageview');
+	</script>	
 </head>
 
 <body>
@@ -64,11 +71,14 @@ All rights reserved
 		</div>
 
 		<div class="row">
-			<div class="span12 centre-block">
+			<div class="span3 offset1">
 				&copy; <?php echo date('Y'); ?> The Bed Booker Ltd.
 			</div>
-		</div>
 
+			<div class="span3 offset4">
+				or <a href="http://eepurl.com/Btq69" target="_blank">Subscribe to our newsletter</a>
+			</div>
+		</div>
 	</div>
 
 </footer>
--- system/application/views/layouts/front.php
+++ system/application/views/layouts/front.php
@@ -20,6 +20,15 @@ All rights reserved
 	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]--> 
 
 	<?php echo $template['metadata']; ?>
+
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-42055908-1', 'bookyourbeds.com');
+	ga('send', 'pageview');
+	</script>	
 </head>
 
 <body>
