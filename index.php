2601bdac59f918bd5352bc35f379c336f93d89aa
Phil Stephens <phil@othertribe.com>
2013-07-11 19:28:09 +0100

    errors

--- index.php
+++ index.php
@@ -63,6 +63,9 @@ switch (ENVIRONMENT)
 	case 'production':
 		error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 		ini_set('display_errors', 0);
+
+		error_reporting(-1);
+		ini_set('display_errors', 1);
 	break;
 
 	default:
